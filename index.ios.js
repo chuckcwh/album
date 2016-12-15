1ca2c64b793ed16a2bbe85f55435e7581a1254c5
Hsu Chia-Wei <chuck@plus65.com.sg>
2016-12-15 20:42:50 +0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+// place code in here for iOS!!!
+
+// import a library to help create a Component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// create a component
+const App = () => (
+  <Header headerText={'Albums'} />
+);
+
+// render it to the device
+AppRegistry.registerComponent('albums', () => App);
